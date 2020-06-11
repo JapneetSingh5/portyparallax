3df21da346bb18be3cac7e321112590153782016
SHIVAM_GUPTA <bestofall39@gmail.com>
2020-06-11 10:24:26 +0530

    particles edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import HeaderIcons from './components/header-icons/header-icons.component'
 import HeaderBackground from './components/header-background/header-background.component'
@@ -69,6 +70,115 @@ function App(props) {
    </ParallaxLayer>
 
    <ParallaxLayer offset={-0.4} speed={-0.2} style={{ display: 'flex', alignItems: 'center', marginLeft: '10%', cursor: 'pointer' }}>
+   <Particles
+       className="particles-bg"
+       params={{
+         particles: {
+  number: {
+    value: 80,
+    density: {
+    enable: true,
+    value_area: 800
+    }
+  },
+  color: {
+    value: "#ffffff"
+  },
+  shape: {
+    type: "circle",
+    stroke: {
+    width: 0,
+    color: "#000000"
+    },
+    polygon: {
+    nb_sides: 5
+    },
+  },
+  opacity: {
+    value: 0.5,
+    random: false,
+    anim: {
+    enable: false,
+    speed: 1,
+    opacity_min: 0.1,
+    sync: false
+    }
+  },
+  size: {
+    value: 3,
+    random: true,
+    "anim": {
+    "enable": false,
+    "speed": 40,
+    "size_min": 0.1,
+    "sync": false
+    }
+  },
+  "line_linked": {
+    "enable": true,
+    "distance": 150,
+    "color": "#ffffff",
+    "opacity": 0.4,
+    "width": 1
+  },
+  "move": {
+    "enable": true,
+    "speed": 6,
+    "direction": "none",
+    "random": false,
+    "straight": false,
+    "out_mode": "out",
+    "bounce": false,
+    "attract": {
+    "enable": false,
+    "rotateX": 600,
+    "rotateY": 1200
+    }
+  }
+  },
+  "interactivity": {
+  "detect_on": "canvas",
+  "events": {
+    "onhover": {
+    "enable": true,
+    "mode": "grab"
+    },
+    "onclick": {
+    "enable": true,
+    "mode": "push"
+    },
+    "resize": true
+  },
+  "modes": {
+    "grab": {
+    "distance": 400,
+    "line_linked": {
+      "opacity": 1
+    }
+    },
+    "bubble": {
+    "distance": 400,
+    "size": 40,
+    "duration": 2,
+    "opacity": 8,
+    "speed": 3
+    },
+    "repulse": {
+    "distance": 200,
+    "duration": 0.4
+    },
+    "push": {
+    "particles_nb": 4
+    },
+    "remove": {
+    "particles_nb": 2
+    }
+  }
+  },
+  "retina_detect": true
+
+       }}
+   />
    <Landing
        name={profile.user.name}
        label={profile.about.label}
--- src/portfolio/landing.jsx
+++ src/portfolio/landing.jsx
@@ -1,65 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Particles from 'react-particles-js';
 
 const Landing = (props) => {
     const { name, label, img, initScroll } = props;
     return (
         <div className="portfolioPage1">
-            <Particles
-                className="particles-bg"
-                params={{
-                    particles: {
-                        number: {
-                            value: 200,
-                            density: {
-                                enable: true,
-                            },
-                        },
-                        size: {
-                            value: 5,
-                            random: true,
-                            anim: {
-                                speed: 4,
-                                size_min: 0.3,
-                            },
-                        },
-                        line_linked: {
-                            enable: false,
-                        },
-                        move: {
-                            random: true,
-                            speed: 1,
-                            direction: 'top',
-                            out_mode: 'out',
-                        },
-                    },
-                    interactivity: {
-                        events: {
-                            onhover: {
-                                enable: true,
-                                mode: 'bubble',
-                            },
-                            onclick: {
-                                enable: true,
-                                mode: 'repulse',
-                            },
-                        },
-                        modes: {
-                            bubble: {
-                                distance: 250,
-                                duration: 2,
-                                size: 0,
-                                opacity: 0,
-                            },
-                            repulse: {
-                                distance: 400,
-                                duration: 4,
-                            },
-                        },
-                    },
-                }}
-            />
             <div className="portfolioPage1Details">
                 <img
                     className="portfolioUserImage"
