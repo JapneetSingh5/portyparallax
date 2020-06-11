6b50d9855b8077f62811a83484db2561e8c3ea73
SHIVAM_GUPTA <bestofall39@gmail.com>
2020-06-11 18:28:02 +0530

    Updates

---
+++ precache-manifest.604d0236f2ba821ef75e3968d932301c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39d3bd1a39849c46b5d26a26e519848",
+    "url": "/portyparallax/index.html"
+  },
+  {
+    "revision": "265459b2a97845763b65",
+    "url": "/portyparallax/static/css/main.5d1aeece.chunk.css"
+  },
+  {
+    "revision": "73ee8f85bd6f17ced744",
+    "url": "/portyparallax/static/js/2.d3060b9d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portyparallax/static/js/2.d3060b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265459b2a97845763b65",
+    "url": "/portyparallax/static/js/main.f3d93e66.chunk.js"
+  },
+  {
+    "revision": "74de24ef639dbfeddc39",
+    "url": "/portyparallax/static/js/runtime-main.73caed2a.js"
+  },
+  {
+    "revision": "305b7b7bf17b8341d4c61d0dc017fb9d",
+    "url": "/portyparallax/static/media/cloud.305b7b7b.svg"
+  },
+  {
+    "revision": "f9c8b12757db632cbd634271b1cd5b10",
+    "url": "/portyparallax/static/media/data_analytics_.f9c8b127.svg"
+  },
+  {
+    "revision": "8c7ae19756b45657b73055a1142be613",
+    "url": "/portyparallax/static/media/data_process.8c7ae197.svg"
+  },
+  {
+    "revision": "54c3adc538f06c7819b16b934f876911",
+    "url": "/portyparallax/static/media/mountain.54c3adc5.svg"
+  },
+  {
+    "revision": "921df655e6f1ce400074caecfdf30605",
+    "url": "/portyparallax/static/media/process_building.921df655.svg"
+  },
+  {
+    "revision": "fafe0d09bc8d6a2b0848c96d52b68f36",
+    "url": "/portyparallax/static/media/sun.fafe0d09.svg"
+  }
+]);
